5ca4eb4f6462680232d7e14bac0be543bc2a4b4d
theeasylearn <theeasylearn@gmail.com>
2025-10-07 21:01:20 +0530

    loop 1

--- 20_nested_decision_2.c
+++ 20_nested_decision_2.c
@@ -4,7 +4,7 @@
 #include<stdio.h>
 void main()
 {
-    int year;
+    int year,reminder1,reminder2,reminder3;
     printf("Enter year");
     scanf("%d",&year);
     if(year<0)
@@ -13,6 +13,23 @@ void main()
     }
     else 
     {
-
+        reminder1 = year % 4; //2000%4 = 0
+        reminder2 = year % 100; //2000 = 0
+        reminder3 = year % 400; //2000 = 0
+        if(reminder1 == 0 && reminder2 != 0)
+        {
+            printf("given year is leap year");
+        }
+        else 
+        {
+            if(reminder2 == 0 && reminder3 == 0)
+            {
+                printf("given year is leap year");
+            }
+            else 
+            {
+                printf("given year is not leap year");
+            }
+        }
     }
 }
---
+++ 21_while_loop_1.c
@@ -0,0 +1,14 @@
+/* write a program to print following series 
+    1   2   3   4   5   6   .... 100
+*/
+#include<stdio.h>
+void main()
+{
+    int number = 0;
+    while(number<100)
+    {
+        number = number + 1; //2
+        printf("%7d",number);//2
+    }
+    
+}
