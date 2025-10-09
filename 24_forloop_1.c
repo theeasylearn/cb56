68da6d00b6545b751c413451d1626c9bd0d30a3e
theeasylearn <theeasylearn@gmail.com>
2025-10-09 21:12:41 +0530

    for loop

---
+++ 24_forloop_1.c
@@ -0,0 +1,21 @@
+// write a program to print ascii table of a to z
+//output 
+// A = 65
+// B = 66
+// Z = 90
+#include<stdio.h>
+void main()
+{
+    //declaration
+    int ascii;
+    char letter;
+    
+
+    for(ascii = 65;ascii<=90;ascii++)
+    {
+        letter = ascii; //copy ascii variable into letter variable
+        printf("\n%c = %d",letter,ascii);
+    }
+
+    
+}
---
+++ 25_forloop_2.c
@@ -0,0 +1,11 @@
+// write a program to make sum of all digit in given amount
+/*  input : 12345 process 1+2+3+4+5 ouput : 15 
+/*  input : 895621 process 8+9+5+6+2+1 ouput : 31
+
+*/
+#include<stdio.h>
+void main()
+{
+    //num = 123
+    
+}
