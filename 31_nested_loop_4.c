b3789bbcec017599ec8a80f48a220473ed5f0169
theeasylearn <theeasylearn@gmail.com>
2025-10-15 20:55:03 +0530

    nested loop

---
+++ 31_nested_loop_4.c
@@ -0,0 +1,24 @@
+/*
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+ */
+#include <stdio.h>
+void main()
+{
+    int hash;
+    for (int row = 5; row >= 1; row--)
+    {
+        for (hash = 1; hash <= row - 1; hash++)
+        {
+            printf(" ");
+        }
+        for (int astrik = 0; astrik <= (5 - hash); astrik++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
