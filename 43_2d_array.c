56e9ad6d898a8d1e75464506987b4d050470fdee
theeasylearn <theeasylearn@gmail.com>
2025-10-31 20:34:19 +0530

    array

--- 43_2d_array.c
+++ 43_2d_array.c
@@ -6,17 +6,29 @@ void main()
     int row, column;
     printf("Enter 1st students marks");
     printf("\n enter 1st subject marks");
+    // this loop is for input and calculate total and average
     for (row = 0; row < 5; row++) // outer for loop
     {
         printf("Enter %d student marks ", row + 1);
         for (column = 0; column < 4; column++) // inner for loop
         {
             printf("\n enter %d subject marks", column + 1);
-            scanf("%d", &marks[row][column]); // 85
+            scanf("%f", &marks[row][column]); // 85
         }
-        //total
+        // total
         marks[row][4] = marks[row][0] + marks[row][1] + marks[row][2] + marks[row][3];
-        marks[row][5] = marks[row][4] / 4; //average
+        marks[row][5] = marks[row][4] / 4; // average
     }
-    
+    for (row = 0; row < 5; row++) // outer for loop
+    {
+        printf("\n %.2f %.2f %.2f %.2f %.2f %.2f", marks[row][0], marks[row][1], marks[row][2], marks[row][3], marks[row][4], marks[row][5]);
+        if (marks[row][0] > 39 && marks[row][1] > 39 && marks[row][2] > 39 && marks[row][3] > 39)
+        {
+            printf(" Pass ");
+        }
+        else
+        {
+            printf(" Fail ");
+        }
+    }
 }
